c78572af890ccbcbd40b2375472b525c9ffed25d
Jonathan Truong <jjtruong90@gmail.com>
2020-06-10 16:24:13 -0700

    Updates

---
+++ precache-manifest.f6510d138bc9eac52dfdf191e0ece56d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f28c57202b1121d4289e1cc20c936f38",
+    "url": "/Practice/index.html"
+  },
+  {
+    "revision": "f2f69f54b5282aca7f29",
+    "url": "/Practice/static/js/2.4116183c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Practice/static/js/2.4116183c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0b53d9b61943ea85f6a",
+    "url": "/Practice/static/js/main.661b82d4.chunk.js"
+  },
+  {
+    "revision": "30d85f98aa6d60500a6e",
+    "url": "/Practice/static/js/runtime-main.4be2d80f.js"
+  }
+]);
